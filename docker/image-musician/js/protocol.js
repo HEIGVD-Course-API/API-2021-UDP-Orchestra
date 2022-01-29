ba186d132d42eb02aaee994f3045cf700a98ef52
Soaperz <crjk.smkr@gmail.com>
2022-01-29 18:24:09 +0100

    Added protocol for both musician and auditor service

---
+++ docker/image-musician/js/protocol.js
@@ -0,0 +1,21 @@
+
+// Multicast address
+const HOST = "224.0.0.1";
+
+// Multicast port
+const PORT = 1337;
+
+// List of available instruments
+const INSTRUMENTS = [
+    ["piano",   "ti-ta-ti"],
+    ["trumpet", "pouet"],
+    ["flute",   "trulu"],
+    ["violin",  "gzi-gzi"],
+    ["drum",    "boum-boum"],
+];
+
+module.exports = {
+    HOST: HOST,
+    PORT: PORT,
+    INSTRUMENTS: INSTRUMENTS
+};
